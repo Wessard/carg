c7321e038931f4b1e73a9666d44af5a469266c2f
mikhail <mishaten19@gmail.com>
2023-02-13 21:47:51 +0500

    menu fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,62 @@
 let intervalId;
-
-document.querySelectorAll('.dropdown-toggle').forEach((el)=> {
-  el.addEventListener('click', el=> {
-    const menu=el.currentTarget.dataset.path;
-    document.querySelectorAll('.dropdown-menu').forEach( el=> {
-      if(!document.querySelector(`[data-target=${menu}]`).classList.contains('open')){
+document.querySelectorAll('.dropdown-toggle').forEach((el) => {
+  el.addEventListener('click', el => {
+    const menu = el.currentTarget.dataset.path;
+    document.querySelectorAll('.dropdown-menu').forEach(el => {
+      if (!document.querySelector(`[data-target=${menu}]`).classList.contains('open')) {
+        document.querySelector(`[data-path=${menu}]`).classList.add('active_st')
         document.querySelector(`[data-target=${menu}]`).classList.add('menu_active')
-        intervalId = setTimeout(()=> {
+        intervalId = setTimeout(() => {
           document.querySelector(`[data-target=${menu}]`).classList.add('open');
-        },0);
+        }, 0);
       }
-      if(document.querySelector(`[data-target=${menu}]`).classList.contains('open')){
+      if (document.querySelector(`[data-target=${menu}]`).classList.contains('open')) {
         clearTimeout(intervalId);
         document.querySelector(`[data-target=${menu}]`).classList.remove('menu_active')
-        intervalId=setTimeout(()=> {
+        intervalId = setTimeout(() => {
           document.querySelector(`[data-target=${menu}]`).classList.remove('open')
-        },0)
+          document.querySelector(`[data-path=${menu}]`).classList.remove('active_st')
+        }, 0)
       }
     });
   })
 })
-
+let close=$('.close_btn');
+let menuw=$('.menu_wrap');
+let menu=$('.menu_burger');
+let menyus=document.querySelectorAll('.dropdown-menu');
+let ddTogle=document.querySelectorAll('.dropdown-toggle');
+$(document).mouseup(function(e){
+  if(!close.is(e.target)&&close.has(e.target).length===0&&
+  !menuw.is(e.target)&&menuw.has(e.target).length===0
+  ){
+    let closeb=document.querySelector('.close_btn')
+    if(closeb.classList.contains('active')){
+    menyus.forEach((el)=> {
+    el.classList.remove('menu_active')||
+    ddTogle.forEach((el)=> {
+    el.classList.remove('active_st')
+    })
+    })
+  }
+    menuw.removeClass('active');
+    menu.removeClass('active');
+  }
+})
 document.addEventListener('DOMContentLoaded', function () {
   $('.menu_burger,.close_btn' || null).click(function (event) {
   $('.menu_burger,.menu_wrap,.close_btn' || null).toggleClass('active')
-    
+  let closeb=document.querySelector('.close_btn')
+  if(closeb.classList.contains('active')){
+    menyus.forEach((el)=> {
+    el.classList.remove('menu_active')||
+    ddTogle.forEach((el)=> {
+      el.classList.remove('active_st')
+    })
+    })
+  }
   })
+
   $('.carg_opa' || null).addClass('_active')
   $('.main_gr' || null).addClass('_active')
   $('.ab_gr' || null).addClass('_active')
@@ -34,7 +65,7 @@ document.addEventListener('DOMContentLoaded', function () {
   let img = document.querySelector('.img_bg') || null;
   let left = document.querySelectorAll('.js_fade_left') || null;
   let right = document.querySelectorAll('.js_fade_right') || null;
-  if(img){
+  if (img) {
     img.classList.add('_active')
 
   }
@@ -49,17 +80,17 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 window.addEventListener('DOMContentLoaded', function () {
-  let home = document.getElementById('home')||null;
-  let non = document.getElementById('non')||null;
-  let none = document.getElementById('none')||null;
-  let back = document.getElementById('back')||null;
-  let btnN = document.getElementById('btn_none')||null;
-  let btnD = document.getElementById('btn_del')||null;
-  let btn_non = document.getElementById('btn_non')||null;
-  let form=document.querySelector('.left_form')||null;
-  let left=document.querySelector('.left_side')||null;
-  let formB=document.querySelector('.form_box')||null;
-  if(home){
+  let home = document.getElementById('home') || null;
+  let non = document.getElementById('non') || null;
+  let none = document.getElementById('none') || null;
+  let back = document.getElementById('back') || null;
+  let btnN = document.getElementById('btn_none') || null;
+  let btnD = document.getElementById('btn_del') || null;
+  let btn_non = document.getElementById('btn_non') || null;
+  let form = document.querySelector('.left_form') || null;
+  let left = document.querySelector('.left_side') || null;
+  let formB = document.querySelector('.form_box') || null;
+  if (home) {
     home.addEventListener('click', function () {
       back.classList.add('active');
       btnN.classList.remove('active');
@@ -77,36 +108,36 @@ window.addEventListener('DOMContentLoaded', function () {
       btnD.classList.add('active');
       btn_non.classList.add('active');
     })
-    btnN.addEventListener('click',(evt)=> {
+    btnN.addEventListener('click', (evt) => {
       const clickCount = evt.target.dataset.clicks = ~~evt.target.dataset.clicks + 1;
-      if (clickCount < 3){
-        let clone=form.cloneNode(true)
+      if (clickCount < 3) {
+        let clone = form.cloneNode(true)
         form.after(clone)
       }
       else {
         alert('Слишком много поездок:)')
       }
     })
-    btnD.addEventListener('click',(event)=> {
-      const clickCount=event.target.dataset.clicks=~~event.target.dataset.clicks+1;
+    btnD.addEventListener('click', (event) => {
+      const clickCount = event.target.dataset.clicks = ~~event.target.dataset.clicks + 1;
       formB.lastChild.remove()
-      if(clickCount>2){
-        btnD.setAttribute('disabled','true')
+      if (clickCount > 2) {
+        btnD.setAttribute('disabled', 'true')
       }
     })
   }
 })
 
 
-  
-const av1=document.getElementById('av1')||null;
-const av2=document.getElementById('av2')||null;
-const ell=document.getElementById('Ellipse')||null;
-if(av1){
-  av1.addEventListener('mouseover',()=> {
+
+const av1 = document.getElementById('av1') || null;
+const av2 = document.getElementById('av2') || null;
+const ell = document.getElementById('Ellipse') || null;
+if (av1) {
+  av1.addEventListener('mouseover', () => {
     ell.classList.add('active_av')
   })
-  av1.addEventListener('mouseover',()=> {
+  av1.addEventListener('mouseover', () => {
     av2.classList.add('active_av')
   })
 
@@ -141,7 +172,7 @@ var mySwiper3 = new Swiper(sliderSelector3, {
   allowTouchMove: false,
   loopedSlides: 1.5,
   slidesPerView: 1,
-  keyboard:true,
+  keyboard: true,
   loop: true,
   speed: 800,
   navigation: {
@@ -154,7 +185,7 @@ var mySwiper4 = new Swiper(sliderSelector4, {
   loop: true,
   allowTouchMove: false,
   centeredSlides: true,
-  loopedSlides:1.5,
+  loopedSlides: 1.5,
   slidesPerView: 1.5,
   speed: 800
 });
